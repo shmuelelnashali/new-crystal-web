a3a12e222016cd697c64732000a2f64bd9fff94e
shmuel <150044213+shmuelelnashali@users.noreply.github.com>
2025-01-26 11:30:02 +0200

    aaaa

--- app/components/missions/TableHead.jsx
+++ app/components/missions/TableHead.jsx
@@ -8,42 +8,41 @@ export default function TableHead({
   columnToSortOn,
   setColumnToSortOn,
 }) {
-  const pathName = usePathname()
+  const pathName = usePathname();
 
-  
-  
   const handleClickArrows = (index, direction) => {
-    setColumnToSortOn({ index, direction }); 
-    const columnName = headTable[index]; 
-    
+    setColumnToSortOn({ index, direction });
+    const columnName = headTable[index];
+
     if (direction === "asc") {
       toast(`סדר עולה הוחל על\n${columnName}`, {
-        icon: '⬆️',
+        icon: "⬆️",
         style: {
-          borderRadius: '10px',
-          background: '#333',
-          color: '#fff',
+          borderRadius: "10px",
+          background: "#333",
+          color: "#fff",
         },
       });
     } else {
       toast(`סדר יורד הוחל על\n${columnName}`, {
-        icon: '⬇️',
+        icon: "⬇️",
         style: {
-          borderRadius: '10px',
-          background: '#333',
-          color: '#fff',
+          borderRadius: "10px",
+          background: "#333",
+          color: "#fff",
         },
       });
     }
   };
-  
 
   return (
     <div className="flex w-full bg-[#EFF3FB] p-1 sticky top-0 z-10">
       <div className="flex w-full bg-blue_color rounded">
         <div className="w-[50px]"></div>
         <div
-          className={`text-[20px] grid grid-cols-${headLength} gap-3 w-full font-semibold leading-6 py-3 text-center items-center text-white`}
+          className={`text-[20px] grid ${
+            pathName.includes("employees") ? "grid-cols-11" : "grid-cols-10"
+          } gap-3 w-full font-semibold leading-6 py-3 text-center items-center text-white`}
         >
           {headTable.map((head, index) => (
             <div
@@ -54,28 +53,34 @@ export default function TableHead({
             >
               <div className={`truncate`}>{head}</div>
 
-              {pathName.includes("mission") && 
-              <div className="mr-1 flex">
-              <MoveDown
-                onClick={() => {
-                  handleClickArrows(index,"desc")
-                }}
-                size={15}
-                color={
-                  columnToSortOn?.index === index  &&columnToSortOn?.direction==='asc'? "gray" : "white"
-                }
-              />
-              <MoveUp
-                onClick={() => {
-                  handleClickArrows(index,"asc") 
-                }}
-                size={15}
-                color={
-                  columnToSortOn?.index === index&&columnToSortOn?.direction==='desc'? "gray" : "white"
-                }
-              />
-            </div>}
-             
+              {pathName.includes("mission") && (
+                <div className="mr-1 flex">
+                  <MoveDown
+                    onClick={() => {
+                      handleClickArrows(index, "desc");
+                    }}
+                    size={15}
+                    color={
+                      columnToSortOn?.index === index &&
+                      columnToSortOn?.direction === "asc"
+                        ? "gray"
+                        : "white"
+                    }
+                  />
+                  <MoveUp
+                    onClick={() => {
+                      handleClickArrows(index, "asc");
+                    }}
+                    size={15}
+                    color={
+                      columnToSortOn?.index === index &&
+                      columnToSortOn?.direction === "desc"
+                        ? "gray"
+                        : "white"
+                    }
+                  />
+                </div>
+              )}
             </div>
           ))}
         </div>
